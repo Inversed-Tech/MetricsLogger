294dc552364367da7fcc34f8d6ea63be04fbd363
Stuart Woodbury <stuart@inversed.tech>
2025-04-26 17:46:10 -0400

    expand e2e test

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -7,15 +7,34 @@ fn test_metrics_logger_integration() {
 
     let recorder = MetricsLogger::new(
         1,
-        |logs| log::debug!("{}", logs),
+        |logs| log::debug!("\n{}", logs),
         |err| log::error!("MetricsLogger error: {}", err),
     );
     metrics::set_global_recorder(recorder).unwrap();
 
     let counter = metrics::counter!("test_counter");
+    let gauge = metrics::gauge!("test_gauge");
+    let histogram = metrics::histogram!("test_histogram");
+
     let handle = std::thread::spawn(move || {
-        for _ in 0..5 {
+        println!("generating logs");
+        for idx in 0..3 {
             counter.increment(1);
+            gauge.increment(idx);
+            histogram.record(idx as f64);
+            std::thread::sleep(Duration::from_secs(1));
+        }
+
+        println!("sleeping. no logs should be generated");
+        std::thread::sleep(Duration::from_secs(3));
+
+        println!(
+            "logging again. testing that metrics with the same name are counted in the same bucket."
+        );
+        for idx in 4..6 {
+            metrics::counter!("test_counter").increment(1);
+            metrics::gauge!("test_gauge").decrement(idx);
+            metrics::histogram!("test_histogram").record(idx as f64);
             std::thread::sleep(Duration::from_secs(1));
         }
     });
