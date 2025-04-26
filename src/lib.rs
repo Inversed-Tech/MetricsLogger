4f5c598dd751242eab9090e83971b7f37b40b110
Stuart Woodbury <stuart@inversed.tech>
2025-04-26 17:10:45 -0400

    update rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,37 +1,41 @@
 //! # metrics-logger
 //!
-//! `metrics-logger` is a crate for logging metrics. It is intended to aid in development and testing,
-//! by allowing the developer to view the metrics in the logs without having to set up an endpoint to
-//! receive the logs over the network.
+//! `metrics-logger` is a crate for logging metrics. It aids development and
+//! testing by allowing developers to view metrics without setting up a network
+//! endpoint.
 //!
-//! This is accomplished by providing an implementation of the `Recorder` trait from the `metrics` crate.
+//! This is done by implementing the `Recorder` trait from the `metrics` crate.
 //!
 //! ## Notes
-//! - the version of `MetricsLogger` matches that of the `metrics` crate which it exports.
-//! - the MetricsLogger struct requires callbacks to avoid potential issues related to the user's
-//!   project using a different version of the `log` or `tracing` crate.
+//! - This crate exports the `metrics` crate to avoid version mismatches. The
+//!   version of `metrics-logger` matches that of the `metrics` crate.
+//! - `MetricsLogger` requires callbacks to avoid issues with different versions
+//!   of the `log` or `tracing` crates in the user's project.
 //!
 //! ## Example
 //! ```rust
 //! use metrics_logger::{metrics, MetricsLogger};
 //!
+//! // MetricsLogger implements the Recorder trait
 //! let recorder = MetricsLogger::new(
 //!     10, // Logging interval in seconds
 //!     |logs| println!("Metrics: {}", logs), // Logging callback
 //!     |err| eprintln!("Error: {}", err),    // Error callback
 //! );
-//! metrics::set_global_recorder(recorder).expect("global recorder can only be set once");
+//!
+//! // This tells the metrics crate to use your Recorder implementation.
+//! metrics::set_global_recorder(recorder).unwrap();
 //! ```
 //!
 //! ## Modules
 //! - `cmd`: Handles commands for updating metrics.
-//! - `handles`: Contains implementations for metric handles (e.g., counters, gauges, histograms).
-//! - `state`: Manages the internal state of metrics and generates logs.
+//! - `handles`: Implements metric handles (e.g., counters, gauges, histograms).
+//! - `state`: Manages metric state and generates logs.
 //!
 //! ## Dependencies
-//! This library re-exports the `metrics` crate to ensure compatibility with the same version used internally.
+//! This library re-exports the `metrics` crate to ensure compatibility with the
+//! same version used internally.
 
-// let the library user have the same version of metrics as this crate.
 pub use metrics;
 
 mod cmd;
